d5990d26cc4d6df6167ebf57a6c8a09db4908e3d
sbcshop3 <129948335+sbcshop3@users.noreply.github.com>
2024-12-16 17:55:56 +0530

    Add files via upload

---
+++ examples/Demo_ImageDisplay/ImgData.h
@@ -0,0 +1,10 @@
+#ifndef _IMGDATA_H_
+#define _IMGDATA_H_
+
+extern const unsigned char enkfi_black[];
+extern const unsigned char enkfi_red[];
+
+extern const unsigned char BIGOffer_IMG[];
+extern const unsigned char HOFFER_IMG[];
+#endif
+/* FILE END */
